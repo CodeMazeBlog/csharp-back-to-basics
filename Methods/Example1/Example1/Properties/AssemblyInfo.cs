8d03d29adca4483cdaa72457e27ce5c2dcba02a9
Marinko <marinkospasojevic@gmail.com>
2018-07-30 07:50:57 +0200

    Methods

--- Methods/Example1/Example1/Properties/AssemblyInfo.cs
+++ Methods/Example1/Example1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FullNameGenerator")]
+[assembly: AssemblyTitle("Example1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FullNameGenerator")]
+[assembly: AssemblyProduct("Example1")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d7de374b-10f6-4338-a478-f7b19a0f0e47")]
+[assembly: Guid("b1b3bb7e-7860-4cd9-ac64-d1df84e0c661")]
 
 // Version information for an assembly consists of the following four values:
 //
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # csharp-back-to-basics
-## Linear Structures
+## Methods in C#
 
-https://code-maze.com/csharp-basics-linear-structures/
+https://code-maze.com/csharp-basics-methods/
