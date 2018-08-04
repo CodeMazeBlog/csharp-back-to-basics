acdd0017640dec5a7693265bf9031ee0ef7726c1
Marinko <marinkospasojevic@gmail.com>
2018-08-04 13:44:36 +0200

    Recursion

---
+++ Recursion/Example1/Example1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+    class Program
+    {
+        public static int CalculateSumRecursively(int n, int m)
+        {
+            int sum = n;
+
+            if(n < m)
+            {
+                n++;
+                return sum += CalculateSumRecursively(n, m);
+            }
+
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter number m: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            int sum = CalculateSumRecursively(n, m);
+
+            Console.WriteLine(sum);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Recursion/Example2/Example2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example2
+{
+    class Program
+    {
+        public static int CountDivisions(double number)
+        {
+            int count = 0;
+
+            if(number % 2 == 0)
+            {
+                count++;
+                number /= 2;
+
+                return count += CountDivisions(number);
+            }
+
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your number: ");
+            double number = Convert.ToDouble(Console.ReadLine());
+
+            int count = CountDivisions(number);
+            Console.WriteLine($"Total number of divisions: {count}");
+
+            Console.ReadKey();
+        }
+    }
+}
