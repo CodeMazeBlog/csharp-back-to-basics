2ab329ecf6c0f001656133773c0937ba26b902e7
Marinko <marinkospasojevic@gmail.com>
2018-09-07 22:51:21 +0200

    Arrays

---
+++ Arrays/Example 2/Example 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_2
+{
+    class Program
+    {
+        public static void PrintMin(params int[] numbers)
+        {
+            int min = numbers[0];
+            for(int i=1; i < numbers.Length; i++)
+            {
+                if(min > numbers[i])
+                {
+                    min = numbers[i];
+                }
+            }
+
+            Console.WriteLine(min);
+        }
+        static void Main(string[] args)
+        {
+            PrintMin(49, 58, 12, 98, 47, 13);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Arrays/Example1/Example1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+    class Program
+    {
+        //array is a reference type so every action in this method will affect original array
+        public static void PopulateArray(int[] numbers)
+        {
+            Random r = new Random();
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = r.Next(1, 101);
+                Console.WriteLine($"The {i+1}. element is {numbers[i]}");
+            }
+        }
+
+        public static void CalculateSum(int[] numbers)
+        {
+            int sum = 0;
+            foreach (int i in numbers)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine($"The sum of all the elements is {sum}");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an array capacity: ");
+            int capacity = Convert.ToInt32(Console.ReadLine());
+
+            int[] numbers = new int[capacity];
+
+            PopulateArray(numbers);
+            Console.WriteLine();
+            CalculateSum(numbers);
+
+            Console.ReadKey();
+        }
+    }
+}
